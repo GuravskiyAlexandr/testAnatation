311005c97770dedf35c6f45d5b8f5790dce6f6f2
Alexsandr <guravskiy79@gmail.com>
2018-04-23 02:51:46 +0300

    asccascasc

--- src/Reflection.java
+++ src/Reflection.java
@@ -20,6 +20,7 @@ public class Reflection {
             Class<?> fieldType = f.getType();
             System.out.println( f.getName()+" = " + fieldType.getTypeName());
         }
+
         try {
             Class<?>[] obj = new Class<?>[]{int.class, int.class}; //указуем два параметра которые мы имеем в классе
             Method method = fClass.getMethod("test", obj );
